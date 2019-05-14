20bacc0df48968ac8e36b5683e81d8c5f4f99721
Leithland Kilburn <kilburnleithland@gmail.com>
2019-05-14 11:08:11 -0400

    Put in vids and took out login flow so internet is required

--- app/src/main/java/com/example/vaibh/refed/MathModule1_Part1.java
+++ app/src/main/java/com/example/vaibh/refed/MathModule1_Part1.java
@@ -30,6 +30,7 @@ public class MathModule1_Part1 extends AppCompatActivity
             String name = intent.getStringExtra("Name");
             Bundle b = this.getIntent().getExtras();
             final String[] array =b.getStringArray("Array");
+            final String topics[];
 
             // Load the math lessons for the given module.
             String achievmentAdapters[] = {"Basic Ratios", "Equivalent Ratios", "Ratios with Double Number Lines",
@@ -37,29 +38,58 @@ public class MathModule1_Part1 extends AppCompatActivity
             switch (name)
             {
                 case "Ratios and Proportional Relationships":
-                    final String ratios[] = {"Basic Ratios", "Equivalent Ratios", "Ratios with Double Number Lines", "Ratios with Tape Diagrams"};
-                    achievmentAdapters = ratios;
+                    final String ratios[] = {"Basic Ratios", "Equivalent Ratios", "Ratios with Double Number Lines", "Ratios with Tape Diagrams", "Comparing Rates",
+                            "Rate Problems", "Equivalent Ratio Word Problems", "Converting decimals to percents and vice-versa", "Converting percents and fractions",
+                            "Findings percents - word problems", "Percent word problems", "Relate fractions, decimals and percents", };
+                    topics = new String[ratios.length];
+                    for (int i = 0; i < ratios.length; i++)
+                    {
+                        topics[i] = ratios[i];
+                    }
+                    achievmentAdapters = topics;
                     CustomAdapterAchievements achievementAdapter1 = new CustomAdapterAchievements(this, ratios);
                     break;
                 case "The Number System":
-                    final String numbers[] = {"Adding and subtracting decimals", "Greatest common factor", "Least common multiple", "Find absolute values"};
-                    achievmentAdapters = numbers;
+                    final String numbers[] = {"Adding and subtracting decimals", "Divide and multiply fractions in word problems",
+                            "Greatest common factor", "Least common multiple", "GCF and LCM - word problems",
+                            "Negative decimals on the number line", "Number opposites", "Ordering negative numbers", "Points in the coordinate plane",
+                            "Distance between points on the coordinate plane", "Find absolute values", "Compare absolute values",
+                            "Compare and order rational numbers", "Numerical inequalities (word problems)"};
+                    topics = new String[numbers.length];
+                    for (int i = 0; i < numbers.length; i++)
+                    {
+                        topics[i] = numbers[i];
+                    }
+                    achievmentAdapters = topics;
                     CustomAdapterAchievements achievementAdapter2 = new CustomAdapterAchievements(this, numbers);
                     break;
                 case "Expressions and Equations":
                     final String expressions[] = {"Parts of algebraic expressions", "Substitution with one and two variables", "Expression value intuition",
-                        "One-step equations (addition and subtraction)", "Graphing inequalities"};
-                    achievmentAdapters = expressions;
+                        "Expressions value intuition - with fractions", "Expressions word problems", "Writing algebraic expressions",
+                        "Writing algebraic expressions - word problems", "Distributive property with variables", "Combining like terms", "Equivalent expressions: distribution + combination",
+                        "One-step equations (addition and subtraction)", "One-step equations (multiplication and division)", "Finding mistakes in one-step equations",
+                        "Finding mistakes in one-step equations", "One-step equations - word problems", "Inequalities with variables", "Graphing inequalities"};
+                    topics = new String[expressions.length];
+                    for (int i = 0; i < expressions.length; i++)
+                    {
+                        topics[i] = expressions[i];
+                    }
+                    achievmentAdapters = topics;
                     CustomAdapterAchievements achievementAdapter3 = new CustomAdapterAchievements(this, expressions);
                     break;
                 case "Geometry":
                     final String geometry[] = {"Parts of a triangle", "Area of right triangles", "Area of a non-right triangle", "Find a missing length",
                         "Area of parallelograms", "Find a missing length when given the area of a parallelogram",
                         "Area of trapezoids", "Area of composite figures (challenge questions)",
-                        "Identify geometric shapes", "Find volume by multiplying area of base by height", "Volume with fraction lengths",
-                        "Volume word problems - with fractions and decimals", "Find surface areas by adding areas of faces", "Nets of polyhedra",
-                        "Introduction to the coordinate plane", "Area and perimeter on the coordinate plane", "Drawing quadrilaterals with coordinates and calculate lengths "};
-                    achievmentAdapters = geometry;
+                        "Identify geometric shapes", "Identifying common 3-D shapes", "Volume word problems - with fractions and decimals",
+                        "Surface area - word problems", "Nets of polyhedra", "Introduction to the coordinate plane", "Area and perimeter on the coordinate plane",
+                        "Drawing quadrilaterals with coordinates and calculate lengths"};
+                    topics = new String[geometry.length];
+                    for (int i = 0; i < geometry.length; i++)
+                    {
+                        topics[i] = geometry[i];
+                    }
+                    achievmentAdapters = topics;
                     CustomAdapterAchievements achievementAdapter4 = new CustomAdapterAchievements(this, geometry);
                     break;
                 case "Statistics and Probability":
@@ -67,16 +97,22 @@ public class MathModule1_Part1 extends AppCompatActivity
                         "Read histograms", "Calculate the mean", "Effects of shifting, adding and removing a data point (on the mean)", "Calculating the median",
                         "Interpreting quartiles", "Interquartile range (IQR)", "Reading box plots", "Creating box plots", "Comparing data displays",
                         "Shape of distributions", "Clusters, gaps, peaks and outliers"};
-                    achievmentAdapters = statistics;
+                    topics = new String[statistics.length];
+                    for (int i = 0; i < statistics.length; i++)
+                    {
+                        topics[i] = statistics[i];
+                    }
+                    achievmentAdapters = topics;
                     CustomAdapterAchievements achievementAdapter5 = new CustomAdapterAchievements(this, statistics);
                     break;
                 default:
+                    topics = new String[0];
                     break;
             }
 
             CustomAdapterAchievements achievmentAdapter = new CustomAdapterAchievements(this, achievmentAdapters);
 
-            ListView listModules = (ListView) findViewById(R.id.lvParts);
+            ListView listModules = findViewById(R.id.lvParts);
             VideoView vvIntro = findViewById(R.id.vvIntro);
             Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.ratiosandpropotions);
             vvIntro.setVideoPath(uri.toString());
@@ -89,9 +125,9 @@ public class MathModule1_Part1 extends AppCompatActivity
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     String module = String.valueOf(parent.getItemAtPosition(position));
-                    Toast.makeText(getApplicationContext(),module,Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),topics[position],Toast.LENGTH_LONG).show();
                     Intent i = new Intent(getApplicationContext(),BRatio1.class);
-                    i.putExtra("Name",array[position]);
+                    i.putExtra("Topic",topics[position]);
                     startActivity(i);
                 }
             });
